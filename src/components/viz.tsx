79c27de97534cef4534e288f40daa04c387d4015
Sophia Saiada <sophia.saiada@gmail.com>
2025-07-27 21:51:14 +0300

    Pave the tunnel for the point

--- src/components/viz.tsx
+++ src/components/viz.tsx
@@ -64,9 +64,20 @@ export const Viz = ({ path }: { path: Step[] }) => {
   // TODO: render only visible rectangles
 
   return (
-    <Stage width={width} height={height}>
+    <Stage width={width} height={height} style={{ backgroundColor: "#4d4d4d" }}>
       <Layer>
         <Group x={0} y={0} ref={layerRef}>
+          <Path
+            data={
+              "M 0 0 " +
+              calculatePolygon(path)
+                .map(({ x, y }) => `L ${x} ${y}`)
+                .join(" ") +
+              " Z"
+            }
+            fill="#0a0a0a"
+          />
+
           {path.map(({ note, x, y, directionOnHit, newDirection }) => (
             <Rect
               key={note.when}
@@ -239,3 +250,67 @@ function updateObstaclesLayerPosition({
   layer?.x(containerXCenter - x);
   layer?.y(containerYCenter - y);
 }
+
+const calculatePolygon = (path: Step[]): { x: number; y: number }[] => {
+  const result = [];
+  const alreadyAddedIndexes = new Set<number>();
+
+  for (let i = 0; i < path.length; ) {
+    const step = path[i];
+    const nextStep = path.at(i + 1);
+    const nextNextStep = path.at(i + 2);
+
+    const isOneStepBeforeBendPoint =
+      nextStep &&
+      nextStep.newDirection.x === -1 * step.directionOnHit.x &&
+      nextStep.newDirection.y === -1 * step.directionOnHit.y;
+
+    const isTwoStepBeforeBendPoint =
+      nextNextStep &&
+      nextNextStep.newDirection.x === -1 * step.newDirection.x &&
+      nextNextStep.newDirection.y === -1 * step.newDirection.y;
+
+    result.push(step);
+    alreadyAddedIndexes.add(i);
+
+    if (isOneStepBeforeBendPoint) {
+      i += 1;
+    } else if (isTwoStepBeforeBendPoint) {
+      i += 3;
+    } else {
+      i += 2;
+    }
+  }
+
+  result.push(
+    getComplementaryMiddleStep(path[path.length - 1], path[path.length - 2])
+  );
+
+  for (let i = path.length - 1; i >= 0; i--) {
+    const step = path[i];
+
+    if (alreadyAddedIndexes.has(i)) {
+      continue;
+    }
+    result.push(step);
+    alreadyAddedIndexes.add(i);
+  }
+
+  result.push(getComplementaryEndStep(path[0], path[1]));
+
+  return result;
+};
+
+const getComplementaryMiddleStep = (lastStep: Step, previousStep: Step) => {
+  if (lastStep.directionOnHit.x === -1 * lastStep.newDirection.x) {
+    return { x: previousStep.x, y: lastStep.y };
+  }
+  return { x: lastStep.x, y: previousStep.y };
+};
+
+const getComplementaryEndStep = (lastStep: Step, previousStep: Step) => {
+  if (lastStep.directionOnHit.x === -1 * lastStep.newDirection.x) {
+    return { x: previousStep.x, y: lastStep.y };
+  }
+  return { x: lastStep.x, y: previousStep.y };
+};
