293067e589662f899be82986dfd479b6a5b8976c
Sophia Saiada <sophia.saiada@gmail.com>
2025-07-29 11:35:36 +0300

    Clearing search box will show default files again

--- src/components/main-screen.tsx
+++ src/components/main-screen.tsx
@@ -36,13 +36,13 @@ export const MainScreen = ({
 
       <SearchBox setSearch={setSearch} isLoading={isLoading} />
 
-      {results && !results.length && (
+      {search && results && !results.length && (
         <div className="text-sm font-body -mt-2 opacity-75 mb-2">
           No results found.
         </div>
       )}
 
-      {(results?.length ? results : MIDI_FILES).map((file) => (
+      {(search && results?.length ? results : MIDI_FILES).map((file) => (
         <button
           key={file.url}
           onClick={isLoading ? undefined : () => onSelectFile(file)}
