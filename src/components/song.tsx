74860715a0963e2721595b01126a04e47c32dd8f
Sophia Saiada <sophia.saiada@gmail.com>
2025-07-30 20:02:57 +0300

    Improve selected song identifying

--- src/components/song.tsx
+++ src/components/song.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { isEqual } from "es-toolkit";
 import type { MidiFile } from "@/types";
 
 export const Song = ({
@@ -14,20 +13,26 @@ export const Song = ({
   isSearching: boolean;
   selectedFile: MidiFile | undefined;
   setSelectedFile: (file: MidiFile | null) => void;
-}) => (
-  <button
-    onClick={isLoading ? undefined : () => setSelectedFile(file)}
-    disabled={isLoading}
-    className={cn(
-      "rounded-md group transition font-body text-left",
-      isLoading ||
-        "cursor-pointer hover:translate-x-1 hover:text-tinted-text hover:text-shadow-dino",
-      isEqual(file, selectedFile) &&
-        "text-tinted-text text-shadow-dino translate-x-1",
-      (isLoading || isSearching) && !isEqual(file, selectedFile) && "opacity-50"
-    )}
-  >
-    <span className="w-4 mr-2">{isEqual(file, selectedFile) ? "⏳" : "♪"}</span>
-    <span>{file.displayName}</span>
-  </button>
-);
+}) => {
+  const isSelected =
+    selectedFile &&
+    file.source === selectedFile.source &&
+    file.id === selectedFile.id;
+
+  return (
+    <button
+      onClick={isLoading ? undefined : () => setSelectedFile(file)}
+      disabled={isLoading}
+      className={cn(
+        "rounded-md group transition font-body text-left",
+        isLoading ||
+          "cursor-pointer hover:translate-x-1 hover:text-tinted-text hover:text-shadow-dino",
+        isSelected && "text-tinted-text text-shadow-dino translate-x-1",
+        (isLoading || isSearching) && !isSelected && "opacity-50"
+      )}
+    >
+      <span className="w-4 mr-2">{isSelected ? "⏳" : "♪"}</span>
+      <span>{file.displayName}</span>
+    </button>
+  );
+};
