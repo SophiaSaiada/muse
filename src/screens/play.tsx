b0cebec8c20568e0f9c5fb2b0613361b9d1d8b9f
Sophia Saiada <sophia.saiada@gmail.com>
2025-07-31 15:27:26 +0300

    Fix loader sizing on mobile

--- src/screens/play.tsx
+++ src/screens/play.tsx
@@ -13,25 +13,27 @@ export const PlayScreen = ({
   <>
     <button
       className={cn(
-        "flex flex-col gap-3 p-8 min-h-dvh justify-center items-center max-w-lg m-auto animate-fade-in",
+        "flex flex-col p-8 min-h-dvh justify-center items-center max-w-lg m-auto animate-fade-in",
         onClickPlay && "cursor-pointer"
       )}
       onClick={onClickPlay}
     >
       <div className="flex flex-row gap-2.5 items-center">
-        {onClickPlay ? (
-          <div className="size-8 text-tinted-text relative shrink-0">
-            <div className="absolute inset-1 rounded-full cursor-pointer bg-tinted-text/75 flex items-center justify-center animate-ping" />
-            <div className="absolute inset-0 rounded-full cursor-pointer bg-[hsl(320,20%,10%)] flex items-center justify-center border-tinted-text/50 border-2 border-solid">
-              <Play className="size-4 text-tinted-text" />
-            </div>
-          </div>
-        ) : (
-          <LoaderPinwheel
-            className="size-7 mx-0.5 text-tinted-text opacity-60 animate-spin"
-            strokeWidth={1.5}
-          />
-        )}
+        <div className="size-8 text-tinted-text relative shrink-0">
+          {onClickPlay ? (
+            <>
+              <div className="absolute inset-1 rounded-full cursor-pointer bg-tinted-text/75 flex items-center justify-center animate-ping" />
+              <div className="absolute inset-0 rounded-full cursor-pointer bg-[hsl(320,20%,10%)] flex items-center justify-center border-tinted-text/50 border-2 border-solid">
+                <Play className="size-4 text-tinted-text" />
+              </div>
+            </>
+          ) : (
+            <LoaderPinwheel
+              className="size-7 text-tinted-text opacity-60 animate-spin m-0.5"
+              strokeWidth={1.5}
+            />
+          )}
+        </div>
         <div className="text-3xl font-headline text-tinted-text inline leading-none text-left">
           {displayName}
         </div>
