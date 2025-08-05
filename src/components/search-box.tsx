cf3b5e1a940a7c06bba64076d96a541cfe8872ea
Sophia Saiada <sophia.saiada@gmail.com>
2025-08-05 19:51:34 +0300

    Add clear button to search box

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, type ChangeEvent, type Dispatch } from "react";
+import { useEffect, useState, type Dispatch } from "react";
 import { debounce } from "es-toolkit";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export const SearchBox = ({
   setSearch,
@@ -16,22 +17,35 @@ export const SearchBox = ({
 
   const [setSearchDebounced] = useState(() => debounce(setSearch, 1000));
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+  const onChange = (value: string) => {
     setValue(value);
-    setSearchDebounced(value);
+    if (value) {
+      setSearchDebounced(value);
+    } else {
+      setSearch(value);
+    }
   };
 
   return (
-    <div className="relative mt-1 mb-2 w-full border border-tinted-text/50 bg-input/30 rounded-md  group focus-within:border-white transition">
+    <div className="relative mt-1 mb-2 w-full border border-tinted-text/50 bg-input/30 rounded-md group focus-within:border-white transition">
       <Search className="size-4 absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none z-0 text-muted-foreground group-focus-within:text-white-text" />
       <input
         type="text"
         placeholder="Search for a song..."
         className="outline-0 grow font-body py-2.5 px-4 pl-10 z-10 relative text-sm w-full"
-        onChange={onChange}
+        onChange={(e) => onChange(e.target.value)}
         value={value}
       />
+      <button
+        onClick={() => onChange("")}
+        className={cn(
+          "w-8 absolute right-0 px-2 top-0 h-full pointer-events-none z-0 text-muted-foreground group-focus-within:text-white-text opacity-0 scale-50 transition flex items-center justify-center",
+          value &&
+            "cursor-pointer pointer-events-auto z-20 opacity-100 scale-100"
+        )}
+      >
+        <X className="size-4" />
+      </button>
     </div>
   );
 };
