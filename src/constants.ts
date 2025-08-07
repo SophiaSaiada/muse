57bd9ab220a81a167b5b30179bc2e262462a2615
Sophia Saiada <sophia.saiada@gmail.com>
2025-08-07 12:33:11 +0300

    Organize constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,44 @@
 import type { MidiFileWithName, VizType } from "@/types";
 
+// Debugging
+export const MUTE = false;
+export const DEBUG_SONG_END = false;
+export const SHOW_PATH = false;
+export const SHOW_BLOCKS = false;
+
+// Color & Size
+export const CIRCLE_COLOR = "#E5438A";
+export const SCALE = 8;
+export const BLOCK_WIDTH = 12;
+export const BLOCK_HEIGHT = 2;
+
+// Music
+export const SONG_START_DELAY_SECONDS = 0.15;
+export const BEATS_VOLUME_FACTOR = 0.1;
+
+// Speed & Camera
+export const SPEED = 200;
+export const CAMERA_FOLLOW_SMOOTHING = 0.2;
+
+// Viz type
+export const VIZ_TYPE_LOCAL_STORAGE_KEY = "vizType" as const;
+export const INITIAL_VIZ_TYPE: VizType = "STARS";
+
+// Circle & Block Animations
+export const BOUNCE_ANIMATION_HALF_TIME = 0.05;
+export const BOUNCE_ANIMATION_SCALE_FACTOR = 0.85;
+export const STAR_COLOR_CHANGE_MAX_DURATION = 0.25;
+export const BLOCK_START_FADE_OUT_AFTER_INDEX = 50;
+export const BLOCK_FADE_MIN_DURATION = 0.75;
+export const BLOCK_START_HUE = 334;
+export const BLOCK_HUE_CHANGE_INDEX_INTERVAL = 100;
+export const BLOCK_HUE_CHANGE_OPEN_ANIMATION_INDEX_INTERVAL = 20;
+
+// Zoom Out Animation
+export const ZOOM_OUT_PADDING_FACTOR = 0.1;
+export const ZOOM_OUT_DURATION_SECONDS = 5;
+export const IMAGE_REVEAL_SMOOTHING = 0.1;
+
 export const MIDI_FILES: MidiFileWithName[] = [
   {
     id: "7Rings",
@@ -39,29 +78,3 @@ export const MIDI_FILES: MidiFileWithName[] = [
     source: "e",
   },
 ];
-
-export const MUTE = false;
-export const DEBUG_SONG_END = false;
-export const SHOW_PATH = false;
-export const SHOW_BLOCKS = false;
-export const CIRCLE_COLOR = "#E5438A";
-export const SCALE = 8;
-export const BLOCK_WIDTH = 12;
-export const BLOCK_HEIGHT = 2;
-export const BOUNCE_ANIMATION_HALF_TIME = 0.05;
-export const BOUNCE_ANIMATION_SCALE_FACTOR = 0.85;
-export const BEATS_VOLUME_FACTOR = 0.1;
-export const SPEED = 200;
-export const VIZ_TYPE_LOCAL_STORAGE_KEY = "vizType" as const;
-export const INITIAL_VIZ_TYPE: VizType = "STARS";
-export const BLOCK_FADE_MIN_DURATION = 0.75;
-export const STAR_COLOR_CHANGE_MAX_DURATION = 0.25;
-export const BLOCK_START_FADE_OUT_AFTER_INDEX = 50;
-export const BLOCK_START_HUE = 334;
-export const BLOCK_HUE_CHANGE_INDEX_INTERVAL = 100;
-export const BLOCK_HUE_CHANGE_OPEN_ANIMATION_INDEX_INTERVAL = 20;
-export const CAMERA_FOLLOW_SMOOTHING = 0.2;
-export const SONG_START_DELAY_SECONDS = 0.15;
-export const ZOOM_OUT_PADDING_FACTOR = 0.1;
-export const ZOOM_OUT_DURATION_SECONDS = 5;
-export const IMAGE_REVEAL_SMOOTHING = 0.1;
